d5378624fcd77ef704cde4cdacc71a1058d9178c
Samantha Morales <samanthamp43@gmail.com>
2026-09-02 14:02:34 -0500

    init repo

---
+++ Main.java
@@ -0,0 +1,9 @@
+void main(String[] args) {
+	String task1 = "Buy milk";
+	String task2 = "Walk the dog";
+
+	System.out.println("My tasks:");
+	System.out.println("1. " + task1);
+	System.out.println("2. " + task2);
+}
+
